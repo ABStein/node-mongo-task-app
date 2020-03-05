47512d8887971875fbc6077416d640510fb5b973
ABStein <abs19stein@gmail.com>
2020-03-05 12:57:28 -0600

    practice CRUD operations in MongoDB

--- mongodb.js
+++ mongodb.js
@@ -15,7 +15,27 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: t
     
     const db = client.db(databaseName);
     
-   db.collection('tasks').updateMany(
+
+    // CREATE
+    db.collection('tasks').addMany((error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(result)
+    })
+
+    // READ
+    db.collection('tasks').find({id: new ObjectId("5e5febb2b9b24cb9ceb3346d")}, (error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(result)
+    })      
+
+    // UPDATE
+    db.collection('tasks').updateMany(
         {
             completed: false
         }, {
@@ -27,5 +47,16 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: t
     }).catch((error) => {
         return console.log('Error!', error);
     })
+
+    // DELETE
+    db.collection('tasks').deleteMany(
+        {
+            task: "Take out the trash",
+        }
+    ).then((resolve) => {
+        console.log('This is resolved.');
+    }).catch((error) => {
+        return console.log('Error!', error);
+    })    
 });
 
